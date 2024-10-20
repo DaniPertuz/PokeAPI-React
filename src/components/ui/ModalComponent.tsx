ce1f975281062b6fb161bc2af7877255c8354508
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-10-19 22:07:54 -0500

    Implement centralized styles

--- src/components/ui/ModalComponent.tsx
+++ src/components/ui/ModalComponent.tsx
@@ -3,6 +3,7 @@ import { Box, Modal, Typography } from '@mui/material';
 import { PokeItemResponse } from '../../interfaces/app-interfaces';
 import { capitalize } from '../../utils/constants';
 import { PokemonSprite } from '../pokemon';
+import { modalContainer } from './styles';
 
 interface Props {
   pokemon: PokeItemResponse;
@@ -15,19 +16,6 @@ const modalRoot = document.getElementById('modal-root') || document.body;
 export const ModalComponent = ({ open, pokemon, handleClose }: Props) => {
   const { abilities, name, sprites, types } = pokemon;
 
-  const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 'auto',
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    borderRadius: 10,
-    boxShadow: 24,
-    p: 4,
-  };
-
   return ReactDOM.createPortal(
     <Modal
       open={open}
@@ -35,7 +23,7 @@ export const ModalComponent = ({ open, pokemon, handleClose }: Props) => {
       aria-labelledby="modal-pokemon-details"
       aria-describedby="modal-pokemon-description"
     >
-      <Box sx={style}>
+      <Box sx={modalContainer}>
         <Typography id="modal-pokemon-title" variant="h4" component="h2">
           {capitalize(name)}
         </Typography>
