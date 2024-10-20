b4aacc9dd895555d6705cebe3c0b42e1ed9cd2c1
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-10-20 12:36:05 -0500

    Apply filter from TextField component

--- src/components/ui/SearchComponent/index.tsx
+++ src/components/ui/SearchComponent/index.tsx
@@ -1,15 +1,31 @@
+import { useContext } from 'react';
 import { CloseOutlined, SearchOutlined } from '@mui/icons-material';
 import { Grid, Box, TextField, IconButton } from '@mui/material';
+import { PokemonContext } from '../../../context/PokemonContext';
 import { useNavbar } from '../../../hooks';
 import { searchContainer, searchTextFieldStyle } from '../styles';
 
 export const SearchComponent = () => {
   const { displayTextField, theme, handleDisplayTextField } = useNavbar();
+  const { searchText, clearSearchText, filterPokemonList } = useContext(PokemonContext);
+
+  const onIconButtonClick = () => {
+    if (displayTextField) clearSearchText();
+
+    handleDisplayTextField();
+  };
   return (
     <Grid item xs={displayTextField ? 7 : 3} sm={4} md={3} lg={2}>
       <Box sx={searchContainer}>
-        <TextField color={'info'} sx={searchTextFieldStyle(theme, displayTextField)} label="Buscar..." variant="standard" />
-        <IconButton color='inherit' onClick={handleDisplayTextField}>
+        <TextField
+          color={'info'}
+          sx={searchTextFieldStyle(theme, displayTextField)}
+          label="Buscar..."
+          variant="standard"
+          value={searchText}
+          onChange={(e) => filterPokemonList(e.target.value)}
+        />
+        <IconButton color='inherit' onClick={onIconButtonClick}>
           {displayTextField ? <CloseOutlined /> : <SearchOutlined />}
         </IconButton>
       </Box>
