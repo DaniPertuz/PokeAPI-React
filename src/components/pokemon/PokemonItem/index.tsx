87aac4f84fcc336e3990d01a58ba087e3c3c7289
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-10-20 11:20:13 -0500

    Implement Avatar to handle fallback image

--- src/components/pokemon/PokemonItem/index.tsx
+++ src/components/pokemon/PokemonItem/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Grid, Card, CardMedia } from '@mui/material';
+import { Avatar, Card, Grid } from '@mui/material';
 import { PokeItemResponse } from '../../../interfaces/app-interfaces';
 import { ModalComponent } from '../../ui/ModalComponent';
 import { pokemonCardStyle, pokemonCardImageStyle } from '../styles';
@@ -18,7 +18,7 @@ export const PokemonItem = ({ pokemon }: Props) => {
     <>
       <Grid item xs={12} sm={6} md={4} lg={3}>
         <Card sx={pokemonCardStyle} onClick={handleOpen}>
-          <CardMedia component={'img'} image={pokemon?.sprites.front_default} sx={pokemonCardImageStyle} />
+          <Avatar src={pokemon?.sprites.front_default} sx={pokemonCardImageStyle} />
         </Card>
       </Grid>
       <ModalComponent open={open} pokemon={pokemon} handleClose={handleClose} />
