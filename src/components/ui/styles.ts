58c19491d794054c9e5513a81c10b05a16fa04cf
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-10-19 22:07:08 -0500

    Styles for UI components

---
+++ src/components/ui/styles.ts
@@ -0,0 +1,12 @@
+export const modalContainer = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 'auto',
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  borderRadius: 10,
+  boxShadow: 24,
+  p: 4,
+};
