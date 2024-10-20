2cde9263b9714338181d703c10abe009afe85754
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-10-19 21:50:26 -0500

    Add components to index file

--- src/components/pokemon/index.tsx
+++ src/components/pokemon/index.tsx
@@ -1,2 +1,4 @@
 export { PokemonGrid } from './PokemonGrid';
+export { PokemonItem } from './PokemonItem';
 export { PokemonLayout } from './PokemonLayout';
+export { PokemonSprite } from './PokemonSprite';
