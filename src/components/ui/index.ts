3722977fed1e6bc15295d5bea93304e4dd72b7de
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-10-21 01:42:57 -0500

    Add custom UI components to index file

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,4 +1,6 @@
+export * from './BodySmall';
 export * from './ErrorMessage';
+export * from './HeaderFour';
 export * from './HeaderTitle';
 export * from './LoadingComponent';
 export * from './ModalComponent';
