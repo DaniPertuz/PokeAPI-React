6a162e82f774ee1c40a55987391e0ba3e7d29b27
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-10-20 21:29:24 -0500

    Set configuration for testing

---
+++ jest.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  transformIgnorePatterns: ['<rootDir>/node_modules/']
+};
