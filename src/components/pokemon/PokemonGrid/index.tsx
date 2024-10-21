bfc0111768502001e4c6191da535243b8c2e62f6
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-10-20 21:28:21 -0500

    Update conditional rendering including possible error

--- src/components/pokemon/PokemonGrid/index.tsx
+++ src/components/pokemon/PokemonGrid/index.tsx
@@ -1,22 +1,28 @@
 import { useContext } from 'react';
 import { Grid } from '@mui/material';
 import { PokemonContext } from '../../../context/PokemonContext';
-import { LoadingComponent } from '../../ui';
+import { ErrorMessage, LoadingComponent } from '../../ui';
 import { PokemonItem } from '../PokemonItem';
 import { PokemonListPagination } from '../PokemonListPagination';
 import { gridContainer } from './styles';
 
 export const PokemonGrid = () => {
-  const { loading, filteredPokemon } = useContext(PokemonContext);
+  const { error, loading, filteredPokemon } = useContext(PokemonContext);
 
   return (
     <Grid container spacing={3} sx={gridContainer}>
       {loading ?
         <LoadingComponent />
         :
-        filteredPokemon.map((pokemon) => <PokemonItem key={pokemon.id} pokemon={pokemon} />)
+        (error && error !== '')
+          ?
+          <ErrorMessage error={error} />
+          :
+          <>
+            {filteredPokemon.map((pokemon) => <PokemonItem key={pokemon.id} pokemon={pokemon} />)}
+            <PokemonListPagination />
+          </>
       }
-      <PokemonListPagination />
     </Grid>
   );
 };
